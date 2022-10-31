e1b993e09c1da7a5adee62d754582e0bb484b35d
Nikita <nicstyle22@gmail.com>
2022-10-31 17:30:35 +0300

    delete import nanoid

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
 import { fetchContacts, addContact, removeContact } from './contacts-operation';
 
 const initialState = {
